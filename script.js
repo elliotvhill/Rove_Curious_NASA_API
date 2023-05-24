36339c9dca77ad5cafdab35ce12652840ea0d07c
Elliot <ellthrowaway2@icloud.com>
2023-05-24 09:48:46 -0400

    added error message if no picture is found

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const apiKey = 'c0k0WgD2uCtdB03NGxld02oBokOdV9SUgKLpRhow'
 const mainContent = document.querySelector('.main-content')
 const button = document.querySelector('#search')
 const marsRoverPic = document.querySelector('#roverPic')
+const description = document.querySelector('#description')
 
 // const i = function getRandomPic() {
 //     return Math.round(Math.random() * 10)
@@ -11,7 +12,14 @@ button.addEventListener('click', async () => {
     let searchInput = document.querySelector('input').value;
     let response = await axios.get(`https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?api_key=${apiKey}&sol=${searchInput}`)
     console.log(response)
-    let marsPic = response.data.photos[0].img_src
-    marsRoverPic.innerHTML = `<img src="${marsPic}" />`
+    let marsPicArray = response.data.photos
+    console.log(marsPicArray)
+    if (marsPicArray.length > 0) {
+        let marsPic = response.data.photos[0].img_src
+        marsRoverPic.innerHTML = `<img src="${marsPic}" />`
+    }
+    else {
+        description.innerHTML = `No photo found. Please try a different Sol.`
+    }
 })
 
